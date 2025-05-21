bd8c61db9c22461c056d7ae2b5c5f4bdc7094d24
Golo258 <golon338@gmail.com>
2025-05-21 19:51:45 +0200

    begging of learning

---
+++ Coding/functions.cpp
@@ -0,0 +1,5 @@
+#include "headers.hpp" 
+
+int substraction(int primary, int secondary) {
+    return primary + secondary + 2; 
+}
---
+++ Coding/headers.hpp
@@ -0,0 +1,7 @@
+
+#ifndef SUBSTRACTING_HPP
+#define SUBSTRACTING_HPP
+
+int substraction(int primary, int secondary); // only declaration 
+
+#endif
