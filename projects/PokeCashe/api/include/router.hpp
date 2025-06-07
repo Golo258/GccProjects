716e9f7e9260bd612e24909b6ac8f13e08cc3654
Grzegorz Golonka <golon338@gmail.com>
2025-06-07 16:40:17 +0100

    start coding some Poke APi

---
+++ projects/PokeCashe/api/include/router.hpp
@@ -0,0 +1,19 @@
+/*--------------- DOCUMENTATION -----------------------
+
+    This class is used to declare the server endpoints
+    registration which will be declare and used in .cpp files
+
+*/
+
+/*----------------- LIBRARIES -----------------------*/
+#pragma once
+#include "lib/httplib.h"
+
+void setup_router(
+    httplib::Server& server
+);
+
+/*----------------- FUNCTIONS -----------------------*/
+/*----------------- VARIABLES -----------------------*/
+/*-----------------  CLASSES -----------------------*/
+/*-----------------  INVOKE -----------------------*/
---
+++ projects/PokeCashe/api/src/main.cpp
@@ -0,0 +1,26 @@
+/*--------------- DOCUMENTATION -----------------------
+
+    This class is used to:
+        - initalize server
+        - register routers
+        - running the server
+*/
+
+/*----------------- LIBRARIES -----------------------*/
+#include <iostream>
+#include "lib/httplib.h"
+#include "include/router.hpp"
+
+/*----------------- FUNCTIONS -----------------------*/
+int main()
+{
+    httplib::Server server;
+    setup_routes(server);
+
+    std::cout << "Server running on https:localhost:8080" << std::endl;
+    server.listen("0.0.0.0", 8080);
+    return 0;
+}
+/*----------------- VARIABLES -----------------------*/
+/*-----------------  CLASSES -----------------------*/
+/*-----------------  INVOKE -----------------------*/
