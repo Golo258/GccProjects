a832ecb71f5de3f511d95f9418f181e6e6b56a91
Grzegorz Golonka <golon338@gmail.com>
2025-06-25 21:13:30 +0100

    clean up and play with namespaces

--- projects/NotesMakerCLI/src/syntaxes.cpp
+++ projects/NotesMakerCLI/src/syntaxes.cpp
@@ -1,46 +1,60 @@
+// -----------------------------
+// Preprocesor i makra w C++
+// -----------------------------
 
-// -makro - mechanizm preprocesora c++
-//  przed kompilacją
-// ifdef - jesli zmienna jest zdefinoiwana
 #include <iostream>
 #include <string>
 #include <chrono>
 #include <thread>
 #include <cstring>
+#include <bits/stdc++.h>
+// -----------------------------
+// Stałe: #define vs constexpr
+// -----------------------------
 
-void logMessage(const std::string& msg);
+#define RADIUS 5                         // Makro - stała bez typu (niepolecane w nowoczesnym C++)
+#define PI_SECOND 3.125                  // Alternatywne makro stałej
+constexpr double PI = 3.14562;           // Stała kompilowana z typem (zalecane)
 
-#define RADIUS 5                        // normal var
-#define TEST_MODE true                  // normal var
-#define SQUARE_FUNC(arg) ((arg * arg))  // function
+// -----------------------------
+// Funkcje makro i operatory makr
+// -----------------------------
+
+#define SQUARE_FUNC(arg) ((arg) * (arg)) // Funkcja makro - zawsze dodawaj nawiasy!
+
+// Łączenie nazw (token pasting)
+#define UNIQUE_ID(prefix) int prefix##__LINE__
+UNIQUE_ID(tmp);  // wygeneruje np. int tmp42;
+
+// Zamiana na string (stringification)
+#define TO_STRING(x) #x
+
+// -----------------------------
+// Warunkowe kompilowanie
+// -----------------------------
+
+#define TEST_MODE
+
 #ifdef TEST_MODE
-#ifndef LOG
-#define LOG(msg) std::cout << "[LOG] " << msg << std::endl
-#endif
+    #ifndef LOG
+        #define LOG(msg) std::cout << "[LOG] " << msg << std::endl
+    #endif
 #else
-#define LOG(TEST_MODE)
+    #define LOG(msg)
 #endif
 
-#define PI_second  3.125
-constexpr double PI = 3.14562;
-// constexpr -- stała kompilowana 
-//  nma ty , jest widoczna w debugrze, 
-#define LOG_ERROR(msg) std::cerr << "[ERROR] " << msg << " (" << __FILE__ << ":" << __LINE__ << ")" << std::endl;
+#undef TEST_MODE  // Usuwa definicję makra TEST_MODE
 
-// #pragma once // zapobiega wielokrotnego includowaniu nagłowków
-// #ifdef, #ifndef, #if, #else, #endif -- warunkowe kompilowanie kodu 
+// -----------------------------
+// Makra predefiniowane (kompilatorowe)
+// -----------------------------
 
-#undef TEST_MODE // zapomina makro 
-
+#define LOG_ERROR(msg) std::cerr << "[ERROR] " << msg << " (" << __FILE__ << ":" << __LINE__ << ")" << std::endl
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
-//  ## łączenie nazwa 
-//  # zamiana na string
-#define UNIQUE_ID(prefix)  int prefix_##__LINE__ // sklei
-UNIQUE_ID(tmp); // do generowania zmiennych
-
-//  sprawdzanie typów na etapie kompilacji
-static_assert(sizeof(int) == 4, "int musi mieć 4 bajty");
+// -----------------------------
+// Pomiar czasu wykonania
+// -----------------------------
 
 #define TIME_BLOCK_START auto start = std::chrono::high_resolution_clock::now();
 #define TIME_BLOCK_END(name) \
@@ -49,21 +63,74 @@ static_assert(sizeof(int) == 4, "int musi mieć 4 bajty");
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() \
               << "ms\n";
 
+// -----------------------------
+// Sprawdzenie kompilacyjne
+// -----------------------------
+
+static_assert(sizeof(int) == 4, "int musi mieć 4 bajty");
+
+// -----------------------------
+// Przestrzenie nazwa -namespacy 
+// -----------------------------
+
+namespace math {
+    constexpr double nRADIUS = 12.5;
+
+    int square(int radius) {
+        return radius * nRADIUS;
+    }
+    
+    int multiply(int principal, int secondary){
+        return principal * (secondary * nRADIUS);
+    }
+}
+
+// typy w namespacach
+namespace geometry {
+    struct Point {
+        int xAxis;
+        int yAxis;
+    };
+
+    Point makePoint(int x, int y) {
+        return Point{x, y};
+    }
+}
+
+namespace text {
+
+    std::string toUpper(const std::string& message){
+        std::string result = message;
+        std::transform(result.begin(), result.end(), result.begin(), ::toupper);
+        return result;
+    }
+    std::string reverse(const std::string& message){
+        std::string result = message;
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+}
+
+// -----------------------------
+// Funkcja main
+// -----------------------------
+
 int main() {
     LOG("Wynik: " << SQUARE_FUNC(RADIUS));
-    // makra debugowe, predefiniowane przez kompilator 
+
     TIME_BLOCK_START
     std::this_thread::sleep_for(std::chrono::milliseconds(150));
-    TIME_BLOCK_END("SLEEP BLOCK")
+    TIME_BLOCK_END("SLEEP BLOCK");
 
-    LOG(__FILE__);
-    LOG(__LINE__);
-    LOG(__func__);
-    LOG(__DATE__); 
-    LOG(__TIME__);
-    LOG(__FILENAME__);
-
-    //  inne fajne 
-    LOG_ERROR("Siemano");
-    
+    LOG(__FILE__);       // pełna ścieżka do pliku
+    LOG(__LINE__);       // numer linii
+    LOG(__func__);       // nazwa funkcji (tutaj: main)
+    LOG(__DATE__);       // data kompilacji
+    LOG(__TIME__);       // czas kompilacji
+    LOG(__FILENAME__);   // tylko nazwa pliku
+    LOG(math::square(12));
+    geometry::Point point = geometry::makePoint(3,12);
+    LOG(point.xAxis + point.yAxis);
+    std::cout << "To upper: " << text::toUpper("manana") << std::endl;
+    std::cout << "Reversed: " << text::reverse("manana") << std::endl;
 }
