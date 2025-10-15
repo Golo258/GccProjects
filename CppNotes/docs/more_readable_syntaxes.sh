a19aa72bdf8bb6dafb992420a110a47e8faf38cb
Grzegorz Golonka <golon338@gmail.com>
2025-10-15 20:37:52 +0100

    create structure for implementing notes

---
+++ CppNotes/docs/more_readable_syntaxes.sh
@@ -0,0 +1,14 @@
+
+Koncepcja:
+    - include/ - deklaracja funckji
+    - src/     - implementaja funkcji
+    - tests/   - testy funkcjonalności
+    - main.cpp - runner wszystkich funkcji
+    - CMakeLists.txt | Makefile - kompilacja wszystkich plików
+    - dodanie nowego hgeadera/cpp - rebuild i test 
+    
+Dokumentacja:
+    od razu pod/nad implementacą funkcji
+    czytelna jasna co sie tam dzieje
+        przykłady, tyip
+    
